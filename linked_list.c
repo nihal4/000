876885c789c9e80d0e574cc2b586cd32f6113b8a
Nihal <sm.nihal4@gmail.com>
2024-10-28 10:11:50 +0600

    kkk

--- linked_list.c
+++ linked_list.c
@@ -3,35 +3,102 @@
 
 struct node{
     int data;
-    struct node* next;
+    struct node * next;
 };
 
-struct node* create_node(int entry_data){
+struct node * create(int entry_data){
     struct node* new_node = (struct node*)malloc(sizeof(struct node));
 
     new_node -> data = entry_data;
     new_node -> next = NULL;
+
     return new_node;
+}
+
+void insert(struct node** head, int entry_data, int position){
+    struct node* new_node = create(entry_data);
+
+    if(position == 1){
+        new_node -> next = *head;
+        *head = new_node;
+        return;
+    }
+
+    struct node* temp = *head;
+    int count = 1;
+
+    while(temp != NULL && count < position-1){
+        temp = temp -> next;
+        count ++;
+    }
+
+
+    if(temp == NULL){
+        temp = *head;
+        while (temp-> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = new_node;
+        
+    }else{
+        new_node -> next = temp ->next;
+        temp -> next = new_node;
+    }
+}
+
+void delete(struct  node** head, int elemnt){
+    struct node* temp = *head;
+    struct node* perv = NULL;
+
+    if(temp != NULL && temp -> data == elemnt){
+        *head = temp -> next;
+        free(temp);
+        return;
+    }
+
+    while(temp != NULL && temp -> data != elemnt){
+        perv = temp;
+        temp = temp -> next;
+    }
+
+    if(temp == NULL) return;
+
+    perv -> next = temp -> next;
+    free(temp);
+}
+
+
+void print_list(struct node* head){
+
+    struct node * temp = head;
+
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    }
+
+    printf("NULL");
+    
 }
 
 
 int main(){
     struct node* head = NULL;
-    struct node* second = NULL;
-    struct node* third = NULL;
-
-    head = create_node(1);
-    second = create_node(2);
-    third = create_node(3);
-
-    head ->next = second;
-    second -> next = third;
 
 
-    struct node* temp = head;
+    insert(&head, 1, 1);
+    insert(&head, 2, 2);
+    insert(&head, 3, 3);
 
-    while(temp!=NULL){
-        printf(" %d ->", temp ->data);
-        temp = temp -> next;
-    }
+    print_list(head);
+
+    delete(&head, 2);
+    delete(&head, 1);
+
+    printf("\n");
+    print_list(head);
+
+
 }
--- sep_2_task_1.c
+++ sep_2_task_1.c
@@ -1,107 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-    int arr_size, prompt, in_index, in_value;
-    //arr size
-    printf("\nenter array size\n");
-    scanf("%d", &arr_size);
-    int arr[arr_size];
-    for(int i = 0; i<arr_size; i++){
-        printf("enter eliment %d:\t", i+1);
-        scanf("%d", &arr[i]);
-    }
+int main() {
+    char sentence[100];
+    int i, j, start, end, length;
 
-    while(prompt!=0){
-        printf("\n\n1.insert\n2.delet\n3.search\n4.change\n0.exit");
-        printf("\nchose:\t");
-        scanf("%d", &prompt);
-         
-        switch(prompt){
-            case 1:
-                //cheking index
-                printf("\ninter index:\t");
-                scanf("%d", &in_index);
-                if((in_index-1)>=0 && (in_index-1) < arr_size){
-                    //ask for new value
-                    printf("\nenter value:\t");
-                    scanf("%d", &in_value);
-                    for(int i = arr_size; i>=(in_index-1); i--){
-                        arr[i] = arr[i-1];
-                    }
-                    arr[(in_index-1)] = in_value;
-                    arr_size++;
+    
+    printf("Enter a sentence: ");
+    fgets(sentence, sizeof(sentence), stdin);
 
-                    // printing new array
+    length = strlen(sentence);
+    start = 0;
 
-                    for(int i = 0; i< arr_size; i++){
-                        printf("%d\t", arr[i]);
-                    }
-                }else{
-                    printf("\ninvalid position\n");
-                }
-                break;
+    
+    for (i = 0; i <= length; i++) {
+        
+        if (sentence[i] == ' ' || sentence[i] == '\0') {
+            end = i - 1;
+
             
-            case 2:
-                //take index
-                printf("\ninter index:\t");
-                scanf("%d", &in_index);
+            for (j = end; j >= start; j--) {
+                printf("%c", sentence[j]);
+            }
 
-                if((in_index-1)>=0 && (in_index-1) < arr_size){
-                    for(int i = (in_index-1); i<arr_size; i++){
-                        arr[i] = arr[i+1];
-                    }
-                    arr_size--;
-                    
-                    // printing new array
+            
+            if (sentence[i] == ' ') {
+                printf(" ");
+            }
 
-                    for(int i = 0; i< arr_size; i++){
-                        printf("%d\t", arr[i]);
-                    }
-                }else{
-                    printf("\ninvalid position\n");
-                }
-                break;
-
-            case 3:
-                //take index
-                printf("\ninter value:\t");
-                scanf("%d", &in_value);
-                int exits_in_array = 0;
-                for(int i =0; i<arr_size; i++){
-                    if(in_value == arr[i]){
-                        //printf("\n%d is in index %d\n", in_value, i);
-                        exits_in_array = 1;
-                    }
-                }
-                if(exits_in_array){
-                    printf("\n%d exist in array", in_value);
-                }else{
-                    printf("\n%d dosen't exist in array", in_value);
-                }
-                break;
-            case 4:
-                printf("\ninter index:\t");
-                scanf("%d", &in_index);
-                if((in_index-1)>=0 && (in_index-1) < arr_size){
-                    //ask for new value
-                    printf("\nenter value:\t");
-                    scanf("%d", &in_value);
-                    arr[in_index-1]= in_value;
-
-
-                    for(int i = 0; i< arr_size; i++){
-                        printf("%d\t", arr[i]);
-                    }
-                }else{
-                    printf("\ninvalid position\n");
-                }
-                break;
-            case 0:
-                printf("\nexiting...\n");
-                break; 
-            default:
-            printf("you have chose invalid option\n");
-            break;
+            
+            start = i + 1;
         }
     }
 
